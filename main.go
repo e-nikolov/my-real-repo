dfe8c0f905cce0d66840767079d033675288e11e
Emil Nikolov <emil.e.nikolov@gmail.com>
2017-10-19 01:14:55 +0200

    dep ignore example with local package

---
+++ main.go
@@ -0,0 +1,7 @@
+package my_real_repo
+
+import "github.com/pkg/errors"
+
+func main() {
+	panic(errors.New("Just a bit of panic"))
+}
---
+++ plugins/fake_plugin/main.go
@@ -0,0 +1,7 @@
+package fake_plugin
+
+import "github.com/go-chi/chi"
+
+func main() {
+	chi.NewRouter()
+}
